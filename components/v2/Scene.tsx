dfab092a114d011e0329798dc3530a07e8d02a07
emilacho <emilacho@hotmail.com>
2026-05-17 17:44:41 +0200

    fix(v2): R43 · remove magenta debug · proxies back to invisible

--- components/v2/Scene.tsx
+++ components/v2/Scene.tsx
@@ -773,12 +773,7 @@ function CoconutHoverCards() {
             }}
           >
             <sphereGeometry args={[0.15, 12, 12]} />
-            <meshBasicMaterial
-              color={review.coconutName === "Coconut_10_43" ? "#ff00ff" : "#000000"}
-              transparent
-              opacity={review.coconutName === "Coconut_10_43" ? 0.7 : 0}
-              depthWrite={false}
-            />
+            <meshBasicMaterial transparent opacity={0} depthWrite={false} />
           </mesh>
           {/* Floating review card · above the coconut · ignores
               occlusion so palm leaves can'\''t hide it */}
